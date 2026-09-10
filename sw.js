2c08f842fe95409bc9cb4ffe2018f120d1035ef4
tatsuyaishizukaai-hash <tatsuyaishizuka.ai@gmail.com>
2026-09-10 20:15:56 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,8 @@
-/* 一度開けば、あとは電波がなくても起動できるようにする */
-const CACHE = "subscalc-v2";
-const CORE = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icon-180.png",
-  "./icon-512.png",
+/* 更新をすぐ拾いつつ、電波がないときはキャッシュで動かす */
+const CACHE = "subscalc-v3";
+
+/* 中身が変わらない外部ライブラリだけ、先に取っておく */
+const VENDOR = [
   "https://unpkg.com/react@18/umd/react.production.min.js",
   "https://unpkg.com/react-dom@18/umd/react-dom.production.min.js",
   "https://unpkg.com/@babel/standalone@7/babel.min.js"
@@ -14,7 +11,7 @@ const CORE = [
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(CACHE)
-      .then((c) => Promise.all(CORE.map((u) => c.add(u).catch(() => {}))))
+      .then((c) => Promise.all(VENDOR.map((u) => c.add(u).catch(() => {}))))
       .then(() => self.skipWaiting())
   );
 });
@@ -28,15 +25,32 @@ self.addEventListener("activate", (e) => {
 });
 
 self.addEventListener("fetch", (e) => {
-  if (e.request.method !== "GET") return;
-  e.respondWith(
-    caches.match(e.request).then((hit) =>
-      hit ||
-      fetch(e.request).then((res) => {
+  const req = e.request;
+  if (req.method !== "GET") return;
+
+  const url = new URL(req.url);
+  const isVendor = url.origin !== self.location.origin;
+
+  /* 外部ライブラリ：キャッシュ優先（バージョン固定なので変わらない） */
+  if (isVendor) {
+    e.respondWith(
+      caches.match(req).then((hit) => hit || fetch(req).then((res) => {
         const copy = res.clone();
-        caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+        caches.open(CACHE).then((c) => c.put(req, copy)).catch(() => {});
         return res;
-      }).catch(() => caches.match("./index.html"))
-    )
+      }))
+    );
+    return;
+  }
+
+  /* 自分のファイル：まずネットを見て、つながらないときだけキャッシュ */
+  e.respondWith(
+    fetch(req)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(req, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(req).then((hit) => hit || caches.match("./index.html")))
   );
 });
